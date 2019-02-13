1f4929ad717c9b905d5e7b58e9384b0503a66526
menghan18 <menghan18@users.noreply.github.com>
2019-02-13 14:53:55 +0800

    change

--- src/main.js
+++ src/main.js
@@ -4,13 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'style/reset.css'
 import 'style/border.css'
 import 'style/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 // 在webpack配置文件中设置目录别名style
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="wrapper">
+  <swiper :options="swiperOption">
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl" alt="去哪儿门票">
+    </swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20191/f3b6ae9446056f5501f2497f1058b253.jpg_750x200_7608aa31.jpg'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1812/d6/daa880b254940402.jpg_750x200_b114308a.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff
+  .wrapper
+    overflow hidden
+    // height 26.7vw
+    width 100%
+    // 高度设为0，以padding填充，防止轮播图下方内容因网络原因抖动
+    height 0
+    padding-bottom 26.7%
+    .swiper-img
+      width 100%
+</style>
